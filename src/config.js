01fb86e823736232667571e1a97e9438171a0071
Widhi Harliyanto <72846823+wiyan17@users.noreply.github.com>
2025-01-28 20:55:53 +0700

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  sessionConfig: {
+    restartOnAuthFail: true,
+    cacheEnabled: false,
+    useChrome: true
+  },
+  aiConfig: {
+    maxRetries: 3,
+    timeout: 30000
+  }
+};
